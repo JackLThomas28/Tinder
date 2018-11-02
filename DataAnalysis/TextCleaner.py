27b92a7004b03ee56046f1f3012dd25915e69db5
Jack Thomas <jackthomas@tltstudentimac.go.usu.edu>
2018-11-02 15:51:39 -0600

    emoji regex

--- DataAnalysis/TextCleaner.py
+++ DataAnalysis/TextCleaner.py
@@ -1,6 +1,6 @@
 import nltk
 import sys
-sys.path.append('/Users/jackthomas/Documents/School/Fall2018/cs4320 - Information Retrieval/Project/Tinder/Helpers/')
+sys.path.append('/Users/jackthomas/Documents/School/Tinder/Helpers')
 import fileIO
 import string
 import re
@@ -8,8 +8,11 @@ import re
 # nltk.download('stopwords')
 
 APOSTRAPHE = '\u2019'
-EMOIJI_REGEX = r'\\u\S{4}'
-EMOTICONS_REGEX = r'[\U0001f601-\U0001f64f]'
+EMOTICONS_REGEX = r'[\U0001f600-\U0001f64f]+'
+DINGBATS_REGEX = r'[\U00002702-\U000027b0]+'
+TRANSPORT_AND_MAP_REGEX = r'[\U0001f680-\U0001f6c0]+'
+ENCLOSED_CHARS_REGEX = r'[\U000024c2-\U0001f251]+'
+MISC_REGEX = r'[\U000000a9-\U0001f999]'
 
 
 def tokenize(data):
@@ -24,15 +27,27 @@ def remove_punctuation(data):
     table = str.maketrans('', '', string.punctuation)
     data = [word.translate(table) for word in data]
     ### Remove apostraphes not caught by string.punctuation
-    data = [word for word in data if word != APOSTRAPHE]
-    ### Remove any empty strings
-    return [word for word in data if word != '']
+    return [word for word in data if word != APOSTRAPHE]
 
 
 def remove_emojis(data):
-    result = [re.search(EMOTICONS_REGEX, word) for word in data]
-    # result = [re.match(EMOIJI_REGEX, word) for word in data]
-    print(result)
+    result = []
+    for word in data:
+        match = []
+        match += re.findall(EMOTICONS_REGEX, word)
+        match += re.findall(ENCLOSED_CHARS_REGEX, word)
+        match += re.findall(DINGBATS_REGEX, word)
+        match += re.findall(TRANSPORT_AND_MAP_REGEX, word)
+        match += re.findall(MISC_REGEX, word)
+        if not match == []:
+            for item in match:
+                word = word.replace(item, '')
+        result.append(word)
+    return result
+
+
+def remove_empty_strings(data):
+    return [word for word in data if word != '']
 
 
 def remove_stop_words(data):
@@ -44,7 +59,8 @@ def clean_text(data, attribute):
     text = [tokenize(data[i][attribute]) for i in range(len(data))]
     text = [make_lowercase(entry) for entry in text]
     text = [remove_punctuation(entry) for entry in text]
-    [remove_emojis(entry) for entry in text]
+    text = [remove_emojis(entry) for entry in text]
+    text = [remove_empty_strings(entry) for entry in text]
     text = [remove_stop_words(entry) for entry in text]
     return text
 
